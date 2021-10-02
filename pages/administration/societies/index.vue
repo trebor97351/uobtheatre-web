8bb3f045bbf73d0a63b1ff31dee801c0cb2a3170
tom <tom.streuli@gmail.com>
2021-10-02 11:58:18 +0000

    start on society

--- pages/administration/societies/index.vue
+++ pages/administration/societies/index.vue
@@ -3,23 +3,6 @@
     <h1 class="text-h1">Your Productions</h1>
     <div class="flex items-end space-x-4">
       <div><t-input placeholder="Search by name" /></div>
-      <div>
-        <label>Status</label
-        ><t-select
-          v-model="productionsStatusFilter"
-          :options="[
-            { value: null, text: 'All' },
-            { value: 'DRAFT', text: 'Draft' },
-            { value: 'PUBLISHED', text: 'Published' },
-            { value: 'CLOSED', text: 'Closed' },
-            { value: 'COMPLETE', text: 'Complete' },
-          ]"
-        />
-      </div>
-      <div>
-        <label>Run Date</label>
-        <t-datepicker v-model="productionsRunDateFilter" class="text-black" />
-      </div>
     </div>
     <card class="mt-6">
       <paginated-table
@@ -31,9 +14,7 @@
         :page-info="productionsData ? productionsData.pageInfo : {}"
       >
         <template #head>
-          <table-head-item>Status</table-head-item>
           <table-head-item>Name</table-head-item>
-          <table-head-item>Society</table-head-item>
           <table-head-item>Dates</table-head-item>
         </template>
         <template #default="slotProps">
@@ -42,9 +23,6 @@
             :key="index"
           >
             <table-row-item>
-              <production-status-badge :production="production" />
-            </table-row-item>
-            <table-row-item>
               <nuxt-link
                 :to="`productions/${production.slug}`"
                 class="font-semibold text-sta-orange hover:text-sta-orange-dark"
@@ -52,7 +30,6 @@
                 {{ production.name }}
               </nuxt-link>
             </table-row-item>
-            <table-row-item>{{ production.society.name }}</table-row-item>
             <table-row-item>
               {{ displayStartEnd(production.start, production.end, 'd MMMM') }}
             </table-row-item>
@@ -71,7 +48,6 @@ import TableHeadItem from '@/components/ui/Tables/TableHeadItem.vue'
 import TableRow from '@/components/ui/Tables/TableRow.vue'
 import TableRowItem from '@/components/ui/Tables/TableRowItem.vue'
 import Card from '@/components/ui/Card.vue'
-import ProductionStatusBadge from '@/components/production/ProductionStatusBadge.vue'
 export default {
   components: {
     PaginatedTable,
@@ -79,7 +55,6 @@ export default {
     TableRowItem,
     Card,
     TableRow,
-    ProductionStatusBadge,
   },
   data() {
     return {
